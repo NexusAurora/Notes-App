71e4c9389d0fb752fd93d31ba693290a8114c3da
NexusAurora <sayantichatterjee28@gmail.com>
2025-01-02 18:07:40 +0530

    added edit route and edit functionality

--- server.py
+++ server.py
@@ -10,11 +10,11 @@ storage = []
 def get_hi():
     return "Hii", 200
 
-@app.route("/items", methods=["GET"])
+@app.route("/posts", methods=["GET"])
 def get_item():
     return jsonify(storage), 200
 
-@app.route("/items", methods=["POST"])
+@app.route("/add", methods=["POST"])
 def add_items():
     item = request.json
     try:
@@ -39,8 +39,8 @@ def add_items():
     except Exception as e:
         return jsonify({"error": "Internal server error", "message": str(e)}), 500
 
-@app.route("/items", methods=["DELETE"])
-def delete_items(item_id):
+@app.route("/delete", methods=["DELETE"])
+def delete_items():
     item_id=request.args.get("item_id")
     # Search for the item with the matching _id
     for index, item in enumerate(storage):
@@ -50,5 +50,31 @@ def delete_items(item_id):
     
     return jsonify({"error": "Item not found"}), 404
 
+@app.route("/edit", methods=["PUT"])
+def edit_item():
+    item_id=request.args.get("item_id")
+    new_item = request.json
+    
+    try:
+        if "title" not in new_item.keys() or "body" not in new_item.keys():
+            return jsonify({"error": "Keys don't exist"}), 400
+        
+        if not isinstance(new_item["title"], str) or not isinstance(new_item["body"], str):
+            return jsonify({"error": "Datatype is not String"}), 400
+        
+        if new_item["title"] == "":
+            return jsonify({"error": "Title cannot be empty"}), 400
+        
+        for index, item in enumerate(storage):
+            if item["_id"] == item_id:
+                storage[index]["title"]=new_item["title"]
+                storage[index]["body"]=new_item["body"]
+                storage[index]["timestamp"]=datetime.now(pytz.utc).strftime('%Y-%m-%d %H:%M:%S')        
+            return jsonify(storage[index]), 200
+        return jsonify({"error": "ID Not found !!", "message": str(e)}), 500
+        
+    except Exception as e:
+        return jsonify({"error": "Internal server error", "message": str(e)}), 500
+    
 if __name__ == "__main__":
     app.run(debug=True)
